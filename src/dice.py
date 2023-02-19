4002548cfab11f7bb246c43b496fb87b9a7c38e5
JojoOchMimi <gbl0083@yahoo.com>
2023-02-19 22:29:46 +0100

    Just add roll()

--- src/dice.py
+++ src/dice.py
@@ -1,8 +1,11 @@
-class Dice:
-    def __init__(self):
-        pass
+import random
 
-    def roll(count: int = 1) -> int | tuple[int]:
+
+class Dice:
+    def __init__(self, num_sides=6):
+        self.num_sides = num_sides
+
+    def roll(self, count: int = 1) -> int | tuple[int]:
         '''Roll a random number, `1` to `6` inclusive.
         
         Parameters:\n
@@ -11,3 +14,7 @@ class Dice:
         Returns:\n
         `int` or `tuple[int]` - containing dice value(s).
         '''
+        rolls = tuple(random.randint(1, self.num_sides) for _ in range(count))
+        if count == 1:
+            return rolls[0]
+        return rolls

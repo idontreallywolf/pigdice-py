8b6244ef2701f9396e742dde91ab8228b11dc82b
idontreallywolf <nedim.kanat.lp@gmail.com>
2023-02-19 14:35:24 +0100

    write docs for roll method in Dice class

--- src/dice.py
+++ src/dice.py
@@ -2,6 +2,12 @@ class Dice:
     def __init__(self):
         pass
 
-    # count - how many dices to roll.
-    def roll(count):
-        pass
+    def roll(count: int = 1) -> int | tuple[int]:
+        '''Roll a random number, `1` to `6` inclusive.
+        
+        Parameters:\n
+        `count`: how many dice to roll. Defaults to `1`.
+
+        Returns:\n
+        `int` or `tuple[int]` - containing dice value(s).
+        '''

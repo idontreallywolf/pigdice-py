522a213cb6c021557a684a254a4a81f61f69241c
idontreallywolf <nedim.kanat.lp@gmail.com>
2023-02-20 02:19:21 +0100

    add unit tests for save_score

--- test/test_highscoreManager.py
+++ test/test_highscoreManager.py
@@ -1,7 +1,7 @@
 """Here is a docstring."""
 import unittest
 from src.highscoreManager import HighscoreManager 
-
+from src.config import config
 
 class Test_highscoreManager(unittest.TestCase):
     """Docs for public class."""
@@ -75,6 +75,40 @@ class Test_highscoreManager(unittest.TestCase):
         self.assertEqual(frodo_score, 32)
         self.assertEqual(smiegol_score, 0)
         self.assertEqual(no_score, None)
+    
+    def test_save_scores(self):
+        highscore_manager = HighscoreManager()
+
+        highscore_manager.set_score_by_name('Obunga', 123)
+        highscore_manager.set_score_by_name('Pleb', 3321)
+        highscore_manager.set_score_by_name('Jimmy', 12)
+        highscore_manager.set_score_by_name('Frodo', 32)
+
+        highscore_manager.save_scores(config['SCORES_FILE_PATH'])
+        highscore_manager._clear_all()
+
+        highscore_list_is_empty = len(highscore_manager._highscores.keys()) == 0
+        self.assertTrue(highscore_list_is_empty)
+
+        highscore_manager.load_scores(config['SCORES_FILE_PATH'])
+
+        highscore_list_is_not_empty = len(highscore_manager._highscores.keys()) == 4
+        self.assertTrue(highscore_list_is_not_empty)
+
+        obung = highscore_manager.get_score_by_name('Obunga')
+        pleb = highscore_manager.get_score_by_name('Pleb')
+        jimmy = highscore_manager.get_score_by_name('Jimmy')
+        frodo = highscore_manager.get_score_by_name('Frodo')
+
+        self.assertIsNotNone(obung)
+        self.assertIsNotNone(pleb)
+        self.assertIsNotNone(jimmy)
+        self.assertIsNotNone(frodo)
+
+        self.assertEqual(obung, 123)
+        self.assertEqual(pleb, 3321)
+        self.assertEqual(jimmy, 12)
+        self.assertEqual(frodo, 32)
 
 
 if __name__ == '__main__':

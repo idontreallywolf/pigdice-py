52b1a6efc8ab53312f6eccf36c2e28a3bda34286
JojoOchMimi <gbl0083@yahoo.com>
2023-02-21 22:19:48 +0100

    complete

--- src/dice.py
+++ src/dice.py
@@ -1,29 +1,27 @@
+"""Part of the game."""
 import random
 
 
 class Dice:
+    """Roll dice."""
+
     num_sides = 6
 
-    
-    def __init__(self):
-        pass
-
-
     def roll(self, count: int = 1) -> int | tuple[int]:
-        '''Roll a random number, `1` to `6` inclusive.
+        """
+        Roll a random number, `1` to `6` inclusive.
 
         Parameters:
         `count`: how many dice to roll. Defaults to `1`.
 
         Returns:
         `int` or `tuple[int]` - containing dice value(s).
-        '''
-        
+        """
         rolls = tuple(random.randint(1, self.num_sides) for _ in range(count))
         if count == 1:
             return rolls[0]
         return rolls
 
     def __str__(self):
-        """Override str method"""
+        """For unittest."""
         return f"Dice with {self.num_sides} sides"
--- test/test_dice.py
+++ test/test_dice.py
@@ -1,28 +1,32 @@
+"""Test dice class."""
 import unittest
+
 from src.dice import Dice
 
 
 class TestDice(unittest.TestCase):
+    """Creat TestDice calss."""
+
     def setUp(self):
-        """creating all the necessary test fixtures\n"""
+        """Dice object."""
         self.dice = Dice()
 
     def test_roll_amount(self):
-        """Test all of the rolls are within range\n"""
+        """Test all of the rolls are within range."""
         rolls = self.dice.roll(5)
         for roll in rolls:
             self.assertTrue(1 <= roll <= self.dice.num_sides, f'Roll {roll} is out of range')
 
     def test_roll_single(self):
-        """Test value type and valid value\n"""
+        """Test value type and valid value."""
         result = self.dice.roll()
         self.assertIsInstance(result, int)
         self.assertGreaterEqual(result, 1)
         self.assertLessEqual(result, self.dice.num_sides)
         self.assertTrue(1 <= result <= 6)
-    
+
     def test_roll_multiple(self):
-        """Test tuple\n"""
+        """Test tuple."""
         count = 4
         result = self.dice.roll(count)
         self.assertIsInstance(result, tuple)
@@ -32,7 +36,7 @@ class TestDice(unittest.TestCase):
             self.assertLessEqual(value, self.dice.num_sides)
 
     def test_exceptions(self):
-        """Test input is not an integer\n"""
+        """Test input is not an integer."""
         with self.assertRaises(TypeError):
             self.dice.roll("Invalid")
         with self.assertRaises(TypeError):
@@ -41,11 +45,11 @@ class TestDice(unittest.TestCase):
             self.dice.roll([1,2,3])
 
     def test_intialization(self):
-        """Test that the object initializes properly\n"""
+        """Test that the object initializes properly."""
         self.assertIsInstance(self.dice, Dice) 
 
     def test_str(self):
-        """Test that str representation works\n"""
+        """Test that str representation works."""
         self.assertEqual(str(self.dice), "Dice with 6 sides")
 
 

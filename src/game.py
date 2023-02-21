6979f172bc3c13603499cb10d6a025c3fb133f6e
idontreallywolf <nedim.kanat.lp@gmail.com>
2023-02-21 06:41:42 +0100

    prepare game class methods

--- src/game.py
+++ src/game.py
@@ -17,3 +17,36 @@ class Game:
         # Stores the index for current player.
         self.current_player = 0
 
+    def get_current_player(self):
+        """Get current player."""
+        # TODO: This method should return the current player.
+        pass
+
+    def roll(self):
+        """Roll dice."""
+        # TODO: This method should implement
+        # rolling dice for current player.
+        pass
+
+    def hold(self):
+        """Hold current score."""
+        # TODO: This method should implement
+        # holding dice for current player.
+        pass
+
+    def has_won(self):
+        """Check whether current player has won the game."""
+        # TODO: This method check
+        # whether the current player has won.
+        pass
+
+    def cheat(self):
+        """Grant maximum score to current player."""
+        # TODO: This method should let
+        # the player reach maximum score to win.
+        pass
+
+    def change_name(self, new_name):
+        # TODO: This method should change the name
+        # of the current player to `new_name`.
+        pass

da968ea451577a747b4b7dad1200e3cda59a673e
idontreallywolf <nedim.kanat.lp@gmail.com>
2023-02-21 06:40:30 +0100

    add game class constructor

--- src/game.py
+++ src/game.py
@@ -1,9 +1,19 @@
 """Module docstring."""
 
+from highscore_manager import HighscoreManager
 # pylint: disable=too-few-public-methods
 
 
 class Game:
-    """
-    Docs for Board Class
-    """
+    """Docs for Game Class."""
+
+    def __init__(self):
+        """Initialize Game."""
+        self.highscore_manager = HighscoreManager()
+
+        # Keeps track of Players who are currently playing.
+        self.players = []
+
+        # Stores the index for current player.
+        self.current_player = 0
+

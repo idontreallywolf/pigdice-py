781f65b4bdd9b0e679b5c7a25f8e180add9d7a92
JojoOchMimi <gbl0083@yahoo.com>
2023-02-24 21:24:42 +0100

    complete ai_player and the test, but not sure how to use the dice.num_sides

--- src/ai_player.py
+++ src/ai_player.py
@@ -1,17 +1,46 @@
 """Represents the AI (Computer) player."""
 
-from player import Player
-from config import CHOICE_ROLL
-
+from src.player import Player
+from src.config import CHOICE_ROLL, CHOICE_HOLD
 
 class AIPlayer(Player):
     """AI Player."""
 
-    def __init__(self):
-        """Initialize Player with name: AI."""
+    def __init__(self, threshold=20):
+        """Initialize Player with name: AI and threshold value."""
         super().__init__('AI')
+        self.threshold = threshold
 
-    def make_choice(self) -> int:
-        """Make a choice using pigdice game strategies."""
-        # TODO: Implement AI strategy
+    def make_choice(self, turn_score: int, oppenet_score: int) -> int:
+        """Decription.
+
+        Make a choice using pigdice game strategies.
+        Parameters:
+        turn_score: the current turn socre of the AI player.
+        opponent_score: the total score of the opponent player.
+        Returns:
+        2 to hold, 1 to roll.
+        """
+        expected_score = self._get_expected_score(turn_score)
+        if expected_score <= self.threshold:
+            return CHOICE_HOLD
         return CHOICE_ROLL
+
+    def _get_expected_score(self, turn_score: int) -> float:
+        """How to do.
+
+        Calulate the expected score for the current turn.
+        Parameters:
+        turn_score: the currenet turn score of the AI player
+
+        Returns:
+        float which is the exptedted score of the current turn.
+        """
+        num_sides = 6
+        # the parameter is from dice class, but i don't know how to fix it
+        except_scord = 0.0
+        for i in range(1, num_sides + 1):
+            p = 1 / num_sides
+            #  Claculate the pdrobabilty of each possible outcome
+            except_scord += (turn_score + i) * p
+        return except_scord
---
+++ test/test_ai_player.py
@@ -0,0 +1,78 @@
+"""Test class."""
+import unittest
+from src.config import CHOICE_HOLD, CHOICE_ROLL
+from src.player import Player
+from src.ai_player import AIPlayer
+
+
+class TestAIplayer(unittest.TestCase):
+    """Do test."""
+
+    def setUp(self):
+        """Set up."""
+        self.player = AIPlayer()
+
+    def tearDown(self):
+        """Tear down."""
+        pass
+
+    def test_make_choice_1(self):
+        """Test the value of return of this function."""
+        expected = CHOICE_HOLD
+        result = self.player.make_choice(7, 15)
+        self.assertEqual(expected, result)
+
+    def test_make_choice_2(self):
+        """Test the value of return of this function."""
+        expected = CHOICE_HOLD
+        result = self.player.make_choice(15, 6)
+        self.assertEqual(expected, result)
+
+    def test_make_choice_3(self):
+        """Test the value of return of this function."""
+        expected = CHOICE_ROLL
+        result = self.player.make_choice(36, 72)
+        self.assertEqual(expected, result)
+
+    def test_make_choice_4(self):
+        """Test the value of return of this function."""
+        expected = CHOICE_ROLL
+        result = self.player.make_choice(72, 24)
+        self.assertEqual(expected, result)
+
+    def test_get_expected_score_1(self):
+        """Test the value of return of this function with turn score 0."""
+        expected = 3.5
+        result = self.player._get_expected_score(0)
+        self.assertEqual(expected, result)
+
+    def test_get_expected_score_2(self):
+        """Test the value of return of this function."""
+        expected = 8.5
+        result = self.player._get_expected_score(5)
+        self.assertEqual(expected, result)
+
+    def test_get_expected_score_2(self):
+        """Test the value of return of this function."""
+        expected = 13.499999999999998
+        result = self.player._get_expected_score(10)
+        self.assertEqual(expected, result)
+
+    def test_get_expected_score_returns_float(self):
+        """Test if _get_expected_score returns float value."""
+        turn_score = 3
+        expected_scord = self.player._get_expected_score(turn_score)
+        self.assertIsInstance(expected_scord, float)
+
+    def test_player_name_is_set_properly(self):
+        """Test if Player name is set properly in constructor."""
+        AIPlayer = self.player.get_name()
+        self.assertEquals(AIPlayer, 'AI')
+
+    def test_threshold_initialized_properly(self):
+        """Test if threshold is initialized properly in constructor."""
+        self.assertEquals(self.player.threshold, 20)
+
+
+if __name__ == '__main__':
+    unittest.main()

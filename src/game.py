5ea461c71b45fefb2444c7f3afea92d38542126b
idontreallywolf <nedim.kanat.lp@gmail.com>
2023-02-26 06:54:09 +0100

    remove has_won method

--- src/game.py
+++ src/game.py
@@ -82,12 +82,6 @@ class Game:
         # holding dice for current player.
         pass
 
-    def has_won(self):
-        """Check whether current player has won the game."""
-        # TODO: This method check
-        # whether the current player has won.
-        pass
-
     def cheat(self):
         """Grant maximum score to current player."""
         # TODO: This method should let
